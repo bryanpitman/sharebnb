088c830fb1c52c30939a00106a0bacd199b086b5
Bryan Pitman <pitman.bryan@gmail.com>
2022-09-13 15:58:12 -0700

    backend loads

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import (
     Flask, render_template, request, flash, redirect, session, g, abort,
 )
 
-from forms import (
-    UserAddForm, UserEditForm, LoginForm, MessageForm, CSRFProtection,
-)
+# from forms import (
+#     UserAddForm, UserEditForm, LoginForm, MessageForm, CSRFProtection,
+# )
 
 from models import (
     db, connect_db, User, Listing, DEFAULT_IMAGE_URL, )
