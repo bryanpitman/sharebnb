14378d59fbd7da70829ca50991e8307f3bd26b79
Bryan Pitman <pitman.bryan@gmail.com>
2022-09-13 20:25:06 -0700

    commented out tests

--- sharebnb-schema.sql
+++ sharebnb-schema.sql
@@ -2,5 +2,5 @@ CREATE TABLE users (
   username VARCHAR(25) PRIMARY KEY,
   password TEXT NOT NULL,
   email TEXT NOT NULL
-    CHECK (position('@' IN email) > 1),
+    -- CHECK (position('@' IN email) > 1),
 );
--- sharebnb.sql
+++ sharebnb.sql
@@ -8,11 +8,11 @@ CREATE DATABASE sharebnb;
 \i sharebnb-schema.sql
 \i sharebnb-seed.sql
 
-\echo 'Delete and recreate sharebnb_test db?'
-\prompt 'Return for yes or control-C to cancel > ' foo
+-- \echo 'Delete and recreate sharebnb_test db?'
+-- \prompt 'Return for yes or control-C to cancel > ' foo
 
-DROP DATABASE sharebnb_test;
-CREATE DATABASE sharebnb_test;
-\connect sharebnb_test
+-- DROP DATABASE sharebnb_test;
+-- CREATE DATABASE sharebnb_test;
+-- \connect sharebnb_test
 
-\i sharebnb-schema.sql
+-- \i sharebnb-schema.sql
