89383d2f82b5d4f06406ed2ce69948afffc1775e
Francis Carino <fgcarino@gmail.com>
2022-09-14 09:10:46 -0700

    add seed

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import os
 from dotenv import load_dotenv
 
 from flask import (
     Flask, render_template, request, flash, redirect, session, g, abort,
 )
-# from flask_debugtoolbar import DebugToolbarExtension
+from flask_debugtoolbar import DebugToolbarExtension
 
 # from forms import (
 #     UserAddForm, UserEditForm, LoginForm, MessageForm, CSRFProtection,
@@ -12,10 +13,20 @@ from flask import (
 from models import (
     db, connect_db, User, Listing, DEFAULT_IMAGE_URL, )
 
+load_dotenv()
+
 app = Flask(__name__)
 
 
-# toolbar = DebugToolbarExtension(app)
+# Get DB_URI from environ variable (useful for production/testing) or,
+# if not set there, use development local db.
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ['DATABASE_URL'].replace("postgres://", "postgresql://"))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 
